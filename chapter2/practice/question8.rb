39cc82a849c4a02115a88d74f5594eb0a76755cb
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2025-07-22 16:52:10 +0900

    chapter2 問題解き終わり

---
+++ chapter2/practice/question8.rb
@@ -0,0 +1,16 @@
+total = 300*2
+puts t
+
+=begin
+エラーメッセージ
+
+question8.rb:2:in '<main>': undefined local variable or method 't' for main (NameError)
+
+puts t
+     ^
+
+→　tないよ
+=end
+
+total = 300*2
+puts total
