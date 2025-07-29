fe870f30791805f0a8c33c5317d2c5b56b6b2e1c
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2025-07-29 17:23:30 +0900

    細かな修正

--- chapter6/practice/question7.rb
+++ chapter6/practice/question7.rb
@@ -1,5 +1,12 @@
+# hash →　keyとvalueをもっているもの
+# keyとvalueのセットをつくりたい！
+
 hash = {}
+
+# 初期値=0
 hash.default = 0
+
+# charsメゾット→文字列(caffelatte)を文字にして、それを一文字ずつ配列に格納
 array = "caffelatte".chars
 array.each do |x|
     hash[x] += 1
